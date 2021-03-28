18fb37b98d98018d777dba3df4d24fe1d4dc89f3
jinxiong <jx.li@igola.com>
2021-03-28 20:43:16 +0800

    init

--- example/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
@@ -31,7 +31,7 @@ public class Producer {
         /*
          * Instantiate with a producer group name.
          */
-        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        DefaultMQProducer producer = new DefaultMQProducer("test_topic");
 
         /*
          * Specify name server addresses.
@@ -48,9 +48,11 @@ public class Producer {
         /*
          * Launch the instance.
          */
+
+        producer.setNamesrvAddr("127.0.0.1:9876");
         producer.start();
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 1; i++) {
             try {
 
                 /*
